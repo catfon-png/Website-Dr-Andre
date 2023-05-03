5999f17f211be4f40bde32fc2df3b040305852d3
Catarina Fonseca <catarinafonseca@catarinas-mbp.kpn>
2023-05-03 13:18:09 +0200

    making the nav bar responsive

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
+import { useRef } from 'react';
 import './Navbar.css'
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom'
 
-import { Link, NavLink, useLocation } from 'react-router-dom'
 
-const CustomLink = ({ to, children} : { to: string; children: React.ReactNode }) => {
+const CustomLink = ({ to, children }: { to: string; children: React.ReactNode }) => {
     const location = useLocation();
     const isActive = location.pathname === to;
     const className = isActive ? 'active' : '';
@@ -10,16 +12,19 @@ const CustomLink = ({ to, children} : { to: string; children: React.ReactNode })
 }
 
 export const Navbar = () => {
-    const path = window.location.pathname
+    const path = window.location.pathname;
+    // const navRef = useRef()
     return (
         <nav className='navbar'>
             <Link to='/' className='navbar-logo'>Dr André</Link>
-            <ul>
+            <ul className='navbar-links'>
                 <li><CustomLink to='/areas'>Áreas de Atuação</CustomLink></li>
                 <li><CustomLink to='/cursos'>Cursos</CustomLink></li>
                 <li><CustomLink to='/sobre'>Sobre</CustomLink></li>
                 <li><CustomLink to='/contactos'>Contactos</CustomLink></li>
+                {/* <button className='navbar-btn navbar-close-btn'> <FaTimes /></button> */}
             </ul>
+            <button className='navbar-ham-menu'> <FaBars/></button>
         </nav>
     )
 }
