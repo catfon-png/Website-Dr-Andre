1237726d169b4ddc4215a8ec1eaa5b7eb0d4aa1f
Catarina Fonseca <catarinaf.96@gmail.com>
2023-05-16 18:20:45 +0200

    contact page is done and responsive! just missing the font

--- frontend/src/components/ContactoAndre/ContactoAndre.tsx
+++ frontend/src/components/ContactoAndre/ContactoAndre.tsx
@@ -15,32 +15,34 @@ type ContactProps = {
     linkedin?: string
 }
 
-export const ContactoAndre = ({foto, titulo, descriçãoFoto, email, iglink, instagram, lilink, linkedin} : ContactProps) => {
+export const ContactoAndre = ({ foto, titulo, descriçãoFoto, email, iglink, instagram, lilink, linkedin }: ContactProps) => {
 
 
     return (
-        <div className='contacto-andre'>
-            <img src={foto} alt={descriçãoFoto} />
-            <h2>{titulo}</h2>
-            <div className="email">
-                <HiOutlineMail size={30} color="#2B4C68"/>
-                <p>{email}</p>
+        <div className='contacto'>
+            <img src={foto} alt={descriçãoFoto} className="contacto-img"/>
+            <div className="contacto-text">
+                <h2 className="contacto-title">{titulo}</h2>
+                <div className="email">
+                    <HiOutlineMail size={32} color="#2B4C68" className='icon' />
+                    <p>{email}</p>
+                </div>
+                <div className="instagram">
+                    <a href={iglink} target="_blank">
+                        <AiFillInstagram size={34} className='icon' />
+                        <p>{instagram}</p>
+                    </a>
+                </div>
+                {lilink ?
+                    <div className="linkedin">
+                        <a href={lilink} target="_blank">
+                            <BsLinkedin size={26} className='icon'/>
+                            <p>{linkedin}</p>
+                        </a>
+                    </div>
+                    :
+                    <div></div>}
             </div>
-            <div className="instagram">
-                <a href={iglink} target="_blank">
-                    <AiFillInstagram size={32} />
-                <p>{instagram}</p>
-                </a>
-            </div>
-            {lilink ? 
-            <div className="linkedin">
-                <a href={lilink} target="_blank">
-                    <BsLinkedin size={28} />
-                <p>{linkedin}</p>
-                </a>
-            </div>
-            : 
-            <div></div> }
         </div>
     )
 }
