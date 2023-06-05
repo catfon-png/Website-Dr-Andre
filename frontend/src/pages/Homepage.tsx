b87bb6ab4313ad610b6e9956f5085078f1285856
Catarina Fonseca <catarinaf.96@gmail.com>
2023-06-05 18:12:45 +0200

    added some animation when inView (in homepage). need to calibrate it

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,16 +1,17 @@
-import React, { useRef } from 'react'
-import implants from '../resources/implants (2).webp'
-import dentist from '../resources/dentist.webp'
-import tongue from '../resources/tongue.webp'
-import { Question } from '../components/Question/Question'
-import { Opa } from '../components/OPA/Opa'
-import { DescriptionCO } from '../components/AreaMD/Descriptions/DescriptionCO'
-import { DescriptionImp } from '../components/AreaMD/Descriptions/DescriptionImp'
-import { DescriptionPO } from '../components/AreaMD/Descriptions/DescriptionPO'
-import { AreaMD } from '../components/AreaMD/AreaMD'
-import '../styles/Homepage.css'
-import { Header } from '../components/Header/Header'
-import { motion, useInView } from "framer-motion"
+import React, { useEffect } from 'react';
+import implants from '../resources/implants (2).webp';
+import dentist from '../resources/dentist.webp';
+import tongue from '../resources/tongue.webp';
+import { Question } from '../components/Question/Question';
+import { Opa } from '../components/OPA/Opa';
+import { DescriptionCO } from '../components/AreaMD/Descriptions/DescriptionCO';
+import { DescriptionImp } from '../components/AreaMD/Descriptions/DescriptionImp';
+import { DescriptionPO } from '../components/AreaMD/Descriptions/DescriptionPO';
+import { AreaMD } from '../components/AreaMD/AreaMD';
+import '../styles/Homepage.css';
+import { Header } from '../components/Header/Header';
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 // import { useInView } from "react-intersection-observer";
 
 export const Homepage = () => {
@@ -19,11 +20,33 @@ export const Homepage = () => {
     [implants, 'Implantologia', <DescriptionImp />],
     [tongue, 'Patologia Oral', <DescriptionPO />]]
 
-  const ref = useRef(null);
-  const isInView = useInView(ref)
+// maybe two refs for two different divs
+  const { ref, inView } = useInView({
+    // threshold: 0.1,
+  });
+
+  const animation = useAnimation();
+
+  useEffect(() => {
+    // console.log("use effect hook, inView = ", inView);
+    if (inView) {
+      animation.start({
+        opacity: 1,
+        x: 0,
+        transition: { delay: 0.2, duration: 0.7 },
+      });
+    }
+    if (!inView) {
+      animation.start({
+        opacity: 0,
+        x: 100,
+        transition: { duration: 0.3 },
+      });
+    }
+  }, [inView]);
 
   return (
-    <div ref={ref}>
+    <div >
       <Header />
       <motion.section className="areas" animate={{ x: ["30%", "0%"], opacity: 1 }}
         transition={{ type: "tween", duration: 1 }}
@@ -39,9 +62,14 @@ export const Homepage = () => {
         animate={{ opacity: isInView ? 1 : 0 }}
         transition={{ duration: 3 }}
         className="content"> */}
-        <hr className="horizontal-line" />
+      <hr className="horizontal-line" />
+      <motion.div ref={ref} animate={animation}>
         <Opa />
+      {/* </motion.div> */}
+      {/* <motion.div animate={animation}> */}
         <Question />
+      </motion.div>
+
       {/* </motion.div> */}
     </div>
   )
